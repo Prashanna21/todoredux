99c0358c07e880f23b4899022e8a89bec0d3a32d
Prashanna21 <Prashnnanewar21@gmail.com>
2024-08-01 08:31:38 +0545

    React

--- React/react-router/src/App.jsx
+++ React/react-router/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header'
+import Footer from "./components/Footer/Footer"
 
 
 function App() {
@@ -10,7 +10,6 @@ function App() {
 
   return (
     <>
-      <Header />
     </>
   )
 }
